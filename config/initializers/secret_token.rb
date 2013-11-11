89f775d86797d2e3f8dacf5aa0b88fb928c610c8
Conacel Elena <conacel.elena@gmail.com>
2013-11-11 15:42:04 +0200

    changed project to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Razboi::Application.config.secret_key_base = '5a80c40908d7c3eeb04253bc8d0fca18a57cdec9a24469baffa5a8d0b44316586c5a4f72181372f37b6a6ef71fc12ad9e14c073c13cd72e96b7bd32de5e86fde'
