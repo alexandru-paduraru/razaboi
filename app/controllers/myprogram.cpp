c71ba88f03486f9bda2362b80fef0d6275a22b5c
Alex Paduraru <axelut@me.com>
2013-11-11 19:11:16 +0200

    conflict fix

--- app/controllers/myprogram.cpp
+++ app/controllers/myprogram.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
-    int a = 1; 
-    string elemente = "{\"numar\":\"7\",\"culoare\":\"B\"}";
-=======
+
 int a = 1; 
     string elemente = "{\"1\":{\"numar\":\"7\",\"culoare\":\"B\"}}";
->>>>>>> bbb531dc0035648b1c0a0c4bb17d9589d105fbf7
+
     cout << elemente <<endl;
 
 }
