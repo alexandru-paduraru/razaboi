ec26af30e45fb939ca85df066521553ee2a2b4ef
Alex Paduraru <axelut@me.com>
2013-11-16 17:25:23 +0200

    nu stiu ce plm de conlfict are ala ca nu i'am facut nimic, sper sa mearga....

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -23,7 +23,7 @@ class GameController < ApplicationController
         carte4[:numar] = 7
         carte4[:culoare] = '&spades;'
 
-        Game.set_cards([carte3, carte3, carte4, carte2], [carte1, carte2,carte3,carte4])
+        Game.set_cards([carte1, carte2],[carte3, carte4])
 
     end
 
@@ -60,16 +60,6 @@ class GameController < ApplicationController
                 
                 if @extra_carti1.any?
                     @message = "Razboi. Ai ramas fara carti."
-<<<<<<< HEAD
-                    Game.carti_jucator2.push(@carte_jucator1)
-                    Game.carti_jucator2.push(@carte_jucator2)
-                    Game.carti_jucator2.concat(@extra_carti2)
-                elsif @extra_carti2.any?
-                    @message = "Razboi. Dusmanu a ramas fara carti."
-                    Game.carti_jucator1.push(@carte_jucator2)
-                    Game.carti_jucator1.push(@carte_jucator1)
-                    Game.carti_jucator1.concat(@extra_carti1)
-=======
                     if @carte_jucator1[:numar] < @extra_carti2.last[:numar]
                         Game.carti_jucator2.push(@carte_jucator1)
                         Game.carti_jucator2.push(@carte_jucator2)
@@ -91,7 +81,7 @@ class GameController < ApplicationController
                         Game.carti_jucator2.concat(@extra_carti1)
                         Game.carti_jucator2.push(@carte_jucator2)
                     end
->>>>>>> 71d6d9abd0026b1a6857f982c4d37b16ee85060b
+
                 elsif @extra_carti1.last[:numar] > @extra_carti2.last[:numar]
                     @message = "Razboi. Vei castiga cartile."
                     Game.carti_jucator1.push(@carte_jucator2)
